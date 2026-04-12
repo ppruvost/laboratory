3954395845f7adfccffbbe160b9753e52b415a94
ppruvost <patrick.pruvost50@gmail.com>
2026-04-12 11:35:45 +0200

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,1 +1,14 @@
-
+export const products = [
+  {
+    cas: "64-17-5",
+    nom: "Éthanol",
+    formule: "C2H6O",
+    categorie: "Solvant",
+    localisation: "B27/ B2-1",
+    pictogramme: "flammable.png",
+    image: "ethanol.jpg",
+    fds: "ethanol.pdf",
+    dangers: ["H225", "H319"],
+    substitution: "Isopropanol"
+  }
+];
