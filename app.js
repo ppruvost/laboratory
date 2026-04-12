542f219a541c5365eb2aa4786db2f4e48315bed6
ppruvost <patrick.pruvost50@gmail.com>
2026-04-12 11:30:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+import { products } from "./data/products.js";
+import { dangerDB } from "./data/dangerDB.js";
+import { save, load } from "./modules/storage.js";
 
+let data = load() || products;
+
+function init() {
+  renderTable(data);
+  fillDangerOptions();
+}
+
+function addProduct(p) {
+  data.push(p);
+  save(data);
+  renderTable(data);
+}
