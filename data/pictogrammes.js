05a43ae5d543ab7888ecdcf0ea37d614dc0ac39e
ppruvost <patrick.pruvost50@gmail.com>
2026-04-12 11:36:20 +0200

    Add pictogrammes data for hazard symbols

---
+++ data/pictogrammes.js
@@ -0,0 +1,42 @@
+const pictogrammes = {
+  H200: "SGH_BombeExplosant.jpg",
+  H201: "SGH_BombeExplosant.jpg",
+  H202: "SGH_BombeExplosant.jpg",
+  H203: "SGH_BombeExplosant.jpg",
+
+  H220: "SGH02_Flamme.jpg",
+  H221: "SGH02_Flamme.jpg",
+  H222: "SGH02_Flamme.jpg",
+  H223: "SGH02_Flamme.jpg",
+  H224: "SGH02_Flamme.jpg",
+  H225: "SGH02_Flamme.jpg",
+  H226: "SGH02_Flamme.jpg",
+
+  H230: "SGH04_BouteilleGaz.jpg",
+  H231: "SGH04_BouteilleGaz.jpg",
+  H280: "SGH04_BouteilleGaz.jpg",
+
+  H290: "SGH05_Corrosion.jpg",
+  H314: "SGH05_Corrosion.jpg",
+  H318: "SGH05_Corrosion.jpg",
+
+  H300: "SGH06_TeteDeMort.jpg",
+  H301: "SGH06_TeteDeMort.jpg",
+  H302: "SGH06_TeteDeMort.jpg",
+  H310: "SGH06_TeteDeMort.jpg",
+  H311: "SGH06_TeteDeMort.jpg",
+  H330: "SGH06_TeteDeMort.jpg",
+
+  H317: "SGH07_PointExclamation.jpg",
+  H319: "SGH07_PointExclamation.jpg",
+  H335: "SGH07_PointExclamation.jpg",
+
+  H340: "SGH08_DangerSante.jpg",
+  H341: "SGH08_DangerSante.jpg",
+  H350: "SGH08_DangerSante.jpg",
+  H360: "SGH08_DangerSante.jpg",
+
+  H400: "SGH09_Environnement.jpg",
+  H410: "SGH09_Environnement.jpg",
+  H411: "SGH09_Environnement.jpg"
+};
