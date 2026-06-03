dfdcfed2e6e3e833fc8d1e2afaacfbbff24bef55
ppruvost <patrick.pruvost50@gmail.com>
2026-06-03 11:24:24 +0200

    Implement dynamic module loading in navigation

--- tp-acoustique/js/navigation.js
+++ tp-acoustique/js/navigation.js
@@ -1,1 +1,39 @@
+const content=
+document.getElementById(
+"content"
+);
 
+document
+.querySelectorAll("nav button")
+
+.forEach(btn=>{
+
+btn.onclick=()=>{
+
+loadModule(
+
+btn.dataset.module
+
+);
+
+};
+
+});
+
+async function loadModule(name){
+
+const html=
+
+await fetch(
+
+`modules/${name}.html`
+
+);
+
+content.innerHTML=
+
+await html.text();
+
+updateProgress();
+
+}
