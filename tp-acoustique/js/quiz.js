98351411ac86e9b8914167780ea4fb7df07e2f1e
ppruvost <patrick.pruvost50@gmail.com>
2026-06-07 14:48:35 +0200

    Simplify quiz setup and add console logs
    Refactor quiz initialization and scoring logic.

--- tp-acoustique/js/quiz.js
+++ tp-acoustique/js/quiz.js
@@ -1,86 +1,11 @@
-window.initQuiz=function(){
-
-const q=[
-
-["Unité sonore ?",
-["Hz","dB","m"],
-1],
-
-["Vitesse du son air ?",
-["340","100","1000"],
-0]
-
-];
-
-let html="";
-
-q.forEach((e,i)=>{
-
-html+=`
-
-<div>
-
-<p>${e[0]}</p>
-
-`;
-
-e[1].forEach((r,j)=>{
-
-html+=`
-
-<label>
-
-<input
-type="radio"
-name="${i}"
-value="${j}">
-
-${r}
-
-</label><br>
-
-`;
-
-});
-
-html+="</div>";
-
-});
-
-quizContainer.innerHTML=
-
-html;
-
-corriger.onclick=()=>{
-
-let s=0;
-
-q.forEach((e,i)=>{
-
-const c=
-
-document.querySelector(
-
-`input[name="${i}"]:checked`
-
+console.log(
+"module quiz externe chargé"
 );
 
-if(
+window.initQuiz=function(){
 
-c &&
-
-Number(c.value)===e[2]
-
-)
-
-s++;
-
-});
-
-score.innerHTML=
-
-`${s}/${q.length}`;
-
-};
+console.log(
+"Quiz playsciences chargé dans iframe"
+);
 
 };
