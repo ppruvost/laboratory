f81852fd90c2bd585113555eb12298cdcf51b086
ppruvost <patrick.pruvost50@gmail.com>
2026-06-25 23:29:06 +0200

    Update import paths for data modules

--- tp-chimie/js/tp01-solutions.js
+++ tp-chimie/js/tp01-solutions.js
@@ -4,11 +4,11 @@ console.log("tp01-solutions.js chargé");
    IMPORTS DATA
    ========================================================== */
 
-import products from "../data/products.js";
-import dangerDB from "../data/dangerDB.js";
-import pictogrammes from "../data/pictogrammes.js";
-import glassware from "../data/glassware.js";
-import laboratoryEquipment from "../data/equipment.js";
+import products from "../../data/products.js";
+import dangerDB from "../../data/dangerDB.js";
+import pictogrammes from "../../data/pictogrammes.js";
+import glassware from "../../data/glassware.js";
+import laboratoryEquipment from "../../data/equipment.js";
 
 /* ==========================================================
    INIT
