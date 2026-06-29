32633e605f1cb6fa22deaec48c3796d9f8687cf6
ppruvost <patrick.pruvost50@gmail.com>
2026-06-29 16:21:49 +0200

    Fix image path for Bécher in glassware.js
    Updated the image path for 'Bécher' in glassware data.

--- data/glassware.js
+++ data/glassware.js
@@ -1,6 +1,6 @@
 const glassware = [
   // Verrerie de base
-  { nom: "Bécher", contenance_ml: "50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "assets/img/glassware/becher.png" },
+  { nom: "Bécher", contenance_ml: "50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "../assets/img/glassware/becher.png" },
   { nom: "Erlenmeyer", contenance_ml: "50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", image: "assets/img/glassware/erlenmeyer.png" },
   { nom: "Fiole jaugée", contenance_ml: "50 / 100 / 200 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "assets/img/glassware/fiole_jaugee.png" },
   { nom: "Éprouvette graduée", contenance_ml: "10 / 25 / 50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D1", image: "assets/img/glassware/eprouvette_graduee.png" },
