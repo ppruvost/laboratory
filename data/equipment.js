eb8c7a3cbca8c1f12e3dbee1b297b56203a7018e
ppruvost <patrick.pruvost50@gmail.com>
2026-07-02 17:11:54 +0200

    Update image file extension for balance equipment

--- data/equipment.js
+++ data/equipment.js
@@ -330,7 +330,7 @@ const laboratoryEquipment = [
         nom: "Balance Jeulin 701 277",
         description: "capacité 2 000 g max",
         lieu: "Salle B27 / étagère C2",
-        image: "assets/img/equipments/balance_jeulin.png",
+        image: "assets/img/equipments/balance_jeulin.jpg",
         categorie: "Dissolution",
         description: "Précision ± 1 g",
         noticeUtilisation: "assets/notice/balance_jeulin.pdf"
