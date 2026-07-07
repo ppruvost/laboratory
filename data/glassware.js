21bdc3fc00bc9c81fe9b8d8982dc3590d76f2b8a
ppruvost <patrick.pruvost50@gmail.com>
2026-07-07 15:09:42 +0200

    Update glassware.js

--- data/glassware.js
+++ data/glassware.js
@@ -1,8 +1,8 @@
 const glassware = [
   // Verrerie de base
-  { nom: "Bécher", contenance_ml: "50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "assets/img/glassware/becher.png" },
-  { nom: "Erlenmeyer", contenance_ml: "50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", image: "assets/img/glassware/erlenmeyer.png" },
-  { nom: "Fiole jaugée", contenance_ml: "50 / 100 / 200 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "assets/img/glassware/fiole_jaugee.png" },
+  { nom: "Bécher", contenance_ml: "50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: ["Dissolution", "pHmétrie"], image: "assets/img/glassware/becher.png" },
+  { nom: "Erlenmeyer", contenance_ml: "50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: "pHmétrie", image: "assets/img/glassware/erlenmeyer.png" },
+  { nom: "Fiole jaugée", contenance_ml: "50 / 100 / 200 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", categorie: ["Dissolution", "pHmétrie"], image: "assets/img/glassware/fiole_jaugee.png" },
   { nom: "Éprouvette graduée", contenance_ml: "10 / 25 / 50 / 100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D1", image: "assets/img/glassware/eprouvette_graduee.png" },
   { nom: "Tube à essai", contenance_ml: "5 / 10 / 20", lieu: "Salle B27 / étagère D1", image: "assets/img/glassware/tube_essai.png" },
 
@@ -12,9 +12,9 @@ const glassware = [
   { nom: "Ballon à col long", contenance_ml: "100 / 250 / 500", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "assets/img/glassware/ballon_col_long.png" },
 
   // Mesure précise
-  { nom: "Burette", contenance_ml: "25 / 50", lieu: "Salle B27 / paillasse E2", image: "assets/img/glassware/burette.png" },
-  { nom: "Pipette graduée", contenance_ml: "1 / 2 / 5 / 10 / 20", lieu: "Salle B27 / paillasse E2", categorie: "Dissolution", image: "assets/img/glassware/pipette_graduee.png" },
-  { nom: "Pipette jaugée", contenance_ml: "5 / 10 / 20 / 25", lieu: "Salle B27 / paillasse E2", categorie: "Dissolution", image: "assets/img/glassware/pipette_jaugee.png" },
+  { nom: "Burette", contenance_ml: "25 / 50", lieu: "Salle B27 / paillasse E2", categorie: "pHmétrie", image: "assets/img/glassware/burette.png" },
+  { nom: "Pipette graduée", contenance_ml: "1 / 2 / 5 / 10 / 20", lieu: "Salle B27 / paillasse E2", categorie: ["Dissolution", "pHmétrie"], image: "assets/img/glassware/pipette_graduee.png" },
+  { nom: "Pipette jaugée", contenance_ml: "5 / 10 / 20 / 25", lieu: "Salle B27 / paillasse E2", categorie: ["Dissolution", "pHmétrie"], image: "assets/img/glassware/pipette_jaugee.png" },
   
   // Transfert / séparation
   { nom: "Ampoule à décanter", contenance_ml: "100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D4", image: "assets/img/glassware/ampoule_decanter.png" },
@@ -36,7 +36,7 @@ const glassware = [
   { nom: "Verre de montre", contenance_ml: "50 / 100", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "assets/img/glassware/verre_montre.png" },
   { nom: "Flacon réactif", contenance_ml: "100 / 250 / 500 / 1000", lieu: "Salle B27 / étagère D3", image: "assets/img/glassware/flacon_reactif.png" },
   { nom: "Flacon compte-gouttes", contenance_ml: "10 / 20 / 50", lieu: "Salle B27 / étagère D3", image: "assets/img/glassware/flacon_compte_goutte.png" },
-  { nom: "Pissette", contenance_ml: "250 / 500", lieu: "Salle B27 / étagère D3", categorie: "Dissolution", image: "assets/img/glassware/pissette.png" },
+  { nom: "Pissette", contenance_ml: "250 / 500", lieu: "Salle B27 / étagère D3", categorie: ["Dissolution", "pHmétrie"], image: "assets/img/glassware/pissette.png" },
 
   // Verrerie spécialisée
   { nom: "Dessiccateur", contenance_ml: "N/A", lieu: "", image: "assets/img/glassware/dessicateur.png" },
